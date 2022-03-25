90e38c86e9a93da43329ac668cce103e4bfcd615
marcelnubiral <93550003+marcelnubiral@users.noreply.github.com>
2022-03-25 17:01:33 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   #key_name = var.key_name
   instance_type = var.instance_type
-  security_groups= [var.security_group]
+  security_groups = [var.security_group]
+  subnet_id = var.subnet_id  
   tags= {
     Name = var.tag_name
   }
